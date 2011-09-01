b5caf60b9696816958a9a9d04a8501279a4b4bc3
daemitus <daemitus@gmail.com>
2011-09-01 15:40:15 -0400

    Fix for issue3, on add to the door scheduler, removes any that already exist

--- src/com/daemitus/lockette/DoorSchedule.java
+++ src/com/daemitus/lockette/DoorSchedule.java
@@ -1,7 +1,9 @@
 package com.daemitus.lockette;
 
-import java.util.PriorityQueue;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.PriorityBlockingQueue;
 import org.bukkit.Bukkit;
 import org.bukkit.Effect;
 import org.bukkit.Location;
@@ -10,7 +12,7 @@ import org.bukkit.block.Block;
 public class DoorSchedule implements Runnable {
 
     private int taskID = -1;
-    private final PriorityQueue<DoorTask> taskList = new PriorityQueue<DoorTask>();
+    private final PriorityBlockingQueue<DoorTask> taskList = new PriorityBlockingQueue<DoorTask>();
 
     public boolean start(final Lockette plugin) {
         if (taskID != -1)
@@ -36,35 +38,45 @@ public class DoorSchedule implements Runnable {
         if (taskList.isEmpty())
             return;
         Long time = System.currentTimeMillis();
-        while (!taskList.isEmpty() && time > taskList.peek().time) {
+        while (!taskList.isEmpty() && time > taskList.peek().time && taskList.peek().block != null) {
             close(taskList.poll());
         }
     }
 
     public void add(Set<Block> set, int delta, boolean isNatural, Location loc) {
-        taskList.add(new DoorTask(set, System.currentTimeMillis() + delta * 1000, isNatural, loc));
+        for (DoorTask task : taskList) {
+            if (set.contains(task.block)) {
+                taskList.remove(task);
+                set.remove(task.block);
+            }
+        }
+        Long time = System.currentTimeMillis();
+        boolean playSound = true;
+        for (Block block : set) {
+            taskList.add(new DoorTask(block, time + delta * 1000, isNatural, playSound));
+            playSound = false;
+        }
     }
 
     private void close(DoorTask task) {
-        for (Block block : task.set)
-            block.setData((byte) (block.getData() ^ 0x4));
-        if ((Config.timerDoorSounds) && (task.isNatural || Config.doorSounds))
-            task.loc.getWorld().playEffect(task.loc, Effect.DOOR_TOGGLE, 0);
+        task.block.setData((byte) (task.block.getData() ^ 0x4));
+        if (task.playSound)
+            if ((Config.timerDoorSounds) && (task.isNatural || Config.doorSounds))
+                task.block.getLocation().getWorld().playEffect(task.block.getLocation(), Effect.DOOR_TOGGLE, 0);
     }
 
     private class DoorTask implements Comparable<DoorTask> {
 
         public final Long time;
-        public final Set<Block> set;
+        public final Block block;
         public final boolean isNatural;
-        public final Location loc;
+        public final boolean playSound;
 
-        public DoorTask(Set<Block> set, Long time, boolean isNatural, Location loc) {
-            this.set = set;
+        public DoorTask(Block block, Long time, boolean isNatural, boolean playSound) {
+            this.block = block;
             this.time = time;
             this.isNatural = isNatural;
-            this.loc = loc;
-
+            this.playSound = playSound;
         }
 
         public int compareTo(DoorTask task) {
