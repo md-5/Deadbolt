1a59990cf2b19640124adca624c2199d731c46fa
daemitus <daemitus@gmail.com>
2011-09-07 02:09:46 -0400

    In soviet russia, version increments you! Not worth recompile and re-upload to devbuk

--- src/com/daemitus/deadbolt/bridge/DeadboltBridge.java
+++ src/com/daemitus/deadbolt/bridge/DeadboltBridge.java
@@ -8,14 +8,16 @@ public interface DeadboltBridge {
 
     /**
      * Verifies that a player is authorized to use a protected block.
+     * Default return should be false if not used.
      * @param player The player to be checked
-     * @param names A List<String> of lines on associated signs
+     * @param names A List<String> of lines on associated signs in lower case
      * @return
      */
     public boolean isAuthorized(Player player, List<String> names);
 
     /**
      * Verifies that a player is allowed to protect blocks
+     * Default return should be true if not used.
      * This should include a reason for denial to the player
      * @param player The player to be checked
      * @param block The sign block being placed
