0ab5d0f3882698f520cfd9d5a138072814d00d88
md_5 <md_5@bigpond.com>
2012-12-02 11:52:34 +1100

    Attempt at working with latest CraftBukkit

--- src/main/java/com/daemitus/deadbolt/Util.java
+++ src/main/java/com/daemitus/deadbolt/Util.java
@@ -6,6 +6,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
+import org.bukkit.craftbukkit.block.CraftBlock;
 
 public final class Util {
 
@@ -13,53 +14,31 @@ public final class Util {
     private static final Pattern PSEUDO_COLOR = Pattern.compile("\\&([0-9a-f])");
 
     public static BlockFace getFacingFromByte(byte b) {
-        switch (b) {
-            case 0x2:
-                return BlockFace.EAST;
-            case 0x3:
-                return BlockFace.WEST;
-            case 0x4:
-                return BlockFace.NORTH;
-            case 0x5:
-                return BlockFace.SOUTH;
-            default:
-                return null;
-        }
+        return CraftBlock.notchToBlockFace(b);
     }
 
     public static byte getByteFromFacing(BlockFace bf) {
-        switch (bf) {
-            case EAST:
-                return 0x2;
-            case WEST:
-                return 0x3;
-            case NORTH:
-                return 0x4;
-            case SOUTH:
-                return 0x5;
-            default:
-                return 0x0;
-        }
+        return (byte) CraftBlock.blockFaceToNotch(bf);
     }
-    
+
     public static String formatForSign(String line, int maxlen) {
-    	line = removeColor(line);
-    	line = line.substring(0, line.length() > maxlen ? maxlen : line.length());
+        line = removeColor(line);
+        line = line.substring(0, line.length() > maxlen ? maxlen : line.length());
         return line;
     }
-    
+
     public static String formatForSign(String line) {
-    	return formatForSign(line, 15);
+        return formatForSign(line, 15);
     }
-    
+
     public static boolean signNameEqualsPlayerName(String signName, String playerName) {
-    	String playerName15 = formatForSign(playerName);
-    	
-    	if (signName.equalsIgnoreCase(playerName15)) {
-    		return true;
-    	}
-    	
-    	return false;
+        String playerName15 = formatForSign(playerName);
+
+        if (signName.equalsIgnoreCase(playerName15)) {
+            return true;
+        }
+
+        return false;
     }
 
     public static Block getSignAttached(Sign signState) {
@@ -67,8 +46,10 @@ public final class Util {
     }
 
     public static String removeColor(String text) {
-    	if (text == null) return null;
-    	return ChatColor.stripColor(text);
+        if (text == null) {
+            return null;
+        }
+        return ChatColor.stripColor(text);
     }
 
     public static String createColor(String text) {
