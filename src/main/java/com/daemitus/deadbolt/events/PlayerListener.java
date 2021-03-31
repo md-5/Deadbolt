1e4f76ae573c1ad7d328476832fc0d2a68a5d610
tofiki <81563784+tofiki@users.noreply.github.com>
2021-03-31 13:20:39 +0200

    Fixes - Part 6?

--- src/main/java/com/daemitus/deadbolt/events/PlayerListener.java
+++ src/main/java/com/daemitus/deadbolt/events/PlayerListener.java
@@ -198,12 +198,32 @@ public class PlayerListener implements Listener {
         }
         switch (block.getType()) {
             case CHEST:
+            case BARREL:
+            case SHULKER_BOX:
+            case WHITE_SHULKER_BOX:
+            case ORANGE_SHULKER_BOX:
+            case MAGENTA_SHULKER_BOX:
+            case LIGHT_BLUE_SHULKER_BOX:
+            case YELLOW_SHULKER_BOX:
+            case LIME_SHULKER_BOX:
+            case PINK_SHULKER_BOX:
+            case GRAY_SHULKER_BOX:
+            case LIGHT_GRAY_SHULKER_BOX:
+            case CYAN_SHULKER_BOX:
+            case PURPLE_SHULKER_BOX:
+            case BLUE_SHULKER_BOX:
+            case BROWN_SHULKER_BOX:
+            case GREEN_SHULKER_BOX:
+            case RED_SHULKER_BOX:
+            case BLACK_SHULKER_BOX:
                 return player.hasPermission(Perm.user_create_chest);
             case TRAPPED_CHEST:
                 return player.hasPermission(Perm.user_create_trapped_chest);
             case DISPENSER:
                 return player.hasPermission(Perm.user_create_dispenser);
             case FURNACE:
+            case BLAST_FURNACE:
+            case SMOKER:
             case BURNING_FURNACE:
                 return player.hasPermission(Perm.user_create_furnace);
             case OAK_DOOR:
